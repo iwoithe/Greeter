25a436068fc4226bfc18c87c68eb55499c34a442
I Woithe <iwoithe@just42.net>
2022-07-17 21:28:08 +0930

    Add the color properties

--- src/framework/ui/uitheme.cpp
+++ src/framework/ui/uitheme.cpp
@@ -1,3 +1,173 @@
 #include "uitheme.h"
 
 using namespace gt::ui;
+
+bool UiTheme::isDark() const
+{
+    return m_isDark;
+}
+
+QColor UiTheme::accentColor() const
+{
+    return m_accentColor;
+}
+
+void UiTheme::setAccentColor(QColor accentColor)
+{
+    if (m_accentColor == accentColor) {
+        return;
+    }
+
+    m_accentColor = accentColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::backgroundPrimaryColor() const
+{
+    return m_backgroundPrimaryColor;
+}
+
+void UiTheme::setBackgroundPrimaryColor(QColor backgroundPrimaryColor)
+{
+    if (m_backgroundPrimaryColor == backgroundPrimaryColor) {
+        return;
+    }
+
+    m_backgroundPrimaryColor = backgroundPrimaryColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::backgroundSecondaryColor() const
+{
+    return m_backgroundSecondaryColor;
+}
+
+void UiTheme::setBackgroundSecondaryColor(QColor backgroundSecondaryColor)
+{
+    if (m_backgroundSecondaryColor == backgroundSecondaryColor) {
+        return;
+    }
+
+    m_backgroundSecondaryColor = backgroundSecondaryColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::buttonColor() const
+{
+    return m_buttonColor;
+}
+
+void UiTheme::setButtonColor(QColor buttonColor)
+{
+    if (m_buttonColor == buttonColor) {
+        return;
+    }
+
+    m_buttonColor = buttonColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::componentColor() const
+{
+    return m_componentColor;
+}
+
+void UiTheme::setComponentColor(QColor componentColor)
+{
+    if (m_componentColor == componentColor) {
+        return;
+    }
+
+    m_componentColor = componentColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::focusColor() const
+{
+    return m_focusColor;
+}
+
+void UiTheme::setFocusColor(QColor focusColor)
+{
+    if (m_focusColor == focusColor) {
+        return;
+    }
+
+    m_focusColor = focusColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::fontPrimaryColor() const
+{
+    return m_fontPrimaryColor;
+}
+
+void UiTheme::setFontPrimaryColor(QColor fontPrimaryColor)
+{
+    if (m_fontPrimaryColor == fontPrimaryColor) {
+        return;
+    }
+
+    m_fontPrimaryColor = fontPrimaryColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::fontSecondaryColor() const
+{
+    return m_fontSecondaryColor;
+}
+
+void UiTheme::setFontSecondaryColor(QColor fontSecondaryColor)
+{
+    if (m_fontSecondaryColor == fontSecondaryColor) {
+        return;
+    }
+
+    m_fontSecondaryColor = fontSecondaryColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::linkColor() const
+{
+    return m_linkColor;
+}
+
+void UiTheme::setLinkColor(QColor linkColor)
+{
+    if (m_linkColor == linkColor) {
+        return;
+    }
+
+    m_linkColor = linkColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::popupBackgroundColor() const
+{
+    return m_popupBackgroundColor;
+}
+
+void UiTheme::setPopupBackgroundColor(QColor popupBackgroundColor)
+{
+    if (m_popupBackgroundColor == popupBackgroundColor) {
+        return;
+    }
+
+    m_popupBackgroundColor = popupBackgroundColor;
+    emit themeChanged();
+}
+
+QColor UiTheme::strokeColor() const
+{
+    return m_strokeColor;
+}
+
+void UiTheme::setStrokeColor(QColor strokeColor)
+{
+    if (m_strokeColor == strokeColor) {
+        return;
+    }
+
+    m_strokeColor = strokeColor;
+    emit themeChanged();
+}
--- src/framework/ui/uitheme.h
+++ src/framework/ui/uitheme.h
@@ -1,6 +1,8 @@
 #ifndef GT_UI_UITHEME_H
 #define GT_UI_UITHEME_H
 
+#include <QColor>
+#include <QFont>
 #include <QObject>
 
 // TODO: Should `iuitheme.h` exist?
@@ -10,6 +12,83 @@ namespace gt::ui
 class UiTheme : public QObject
 {
     Q_OBJECT
+
+    Q_PROPERTY(bool isDark READ isDark WRITE setIsDark NOTIFY themeChanged)
+
+    Q_PROPERTY(QColor accentColor READ accentColor WRITE setAccentColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor backgroundPrimaryColor READ backgroundPrimaryColor WRITE setBackgroundPrimaryColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor backgroundSecondaryColor READ backgroundSecondaryColor WRITE setBackgroundSecondaryColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor buttonColor READ buttonColor WRITE setButtonColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor componentColor READ componentColor WRITE setComponentColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor focusColor READ focusColor WRITE setFocusColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor fontPrimaryColor READ fontPrimaryColor WRITE setFontPrimaryColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor fontSecondaryColor READ fontSecondaryColor WRITE setFontSecondaryColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor linkColor READ linkColor WRITE setLinkColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor popupBackgroundColor READ popupBackgroundColor WRITE setPopupBackgroundColor NOTIFY themeChanged)
+    Q_PROPERTY(QColor strokeColor READ strokeColor WRITE setStrokeColor NOTIFY themeChanged)
+
+public:
+    bool isDark() const;
+    void setIsDark(bool isDark);
+
+    QColor accentColor() const;
+    void setAccentColor(QColor accentColor);
+
+    QColor backgroundPrimaryColor() const;
+    void setBackgroundPrimaryColor(QColor backgroundPrimaryColor);
+
+    QColor backgroundSecondaryColor() const;
+    void setBackgroundSecondaryColor(QColor backgroundSecondaryColor);
+
+    QColor buttonColor() const;
+    void setButtonColor(QColor buttonColor);
+
+    QColor componentColor() const;
+    void setComponentColor(QColor componentColor);
+
+    QColor focusColor() const;
+    void setFocusColor(QColor focusColor);
+
+    QColor fontPrimaryColor() const;
+    void setFontPrimaryColor(QColor fontPrimaryColor);
+
+    QColor fontSecondaryColor() const;
+    void setFontSecondaryColor(QColor fontSecondaryColor);
+
+    QColor linkColor() const;
+    void setLinkColor(QColor linkColor);
+
+    QColor popupBackgroundColor() const;
+    void setPopupBackgroundColor(QColor popupBackgroundColor);
+
+    QColor strokeColor() const;
+    void setStrokeColor(QColor strokeColor);
+
+private:
+    // TODO: Should this be named "UiState" instead?
+    struct StyleState {
+        bool enabled = false;
+        bool hovered = false;
+        bool pressed = false;
+        bool focused = false;
+    };
+
+    bool m_isDark;
+
+    QColor m_accentColor;
+    QColor m_backgroundPrimaryColor;
+    QColor m_backgroundSecondaryColor;
+    QColor m_buttonColor;
+    QColor m_componentColor;
+    QColor m_focusColor;
+    QColor m_fontPrimaryColor;
+    QColor m_fontSecondaryColor;
+    QColor m_linkColor;
+    QColor m_popupBackgroundColor;
+    QColor m_strokeColor;
+
+signals:
+    void themeChanged();
 };
 }
 
